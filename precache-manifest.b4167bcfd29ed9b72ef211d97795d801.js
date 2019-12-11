4982ce45e1e3595e64a4c277f6106218b05d2098
Koen Vendrik <k.vendrik@gmail.com>
2019-12-11 18:21:57 -0500

    Updates

---
+++ precache-manifest.b4167bcfd29ed9b72ef211d97795d801.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d4a2fbd963da444822a83777e3d6e46",
+    "url": "/polaris-ml/index.html"
+  },
+  {
+    "revision": "537a8faf697f654e1a97",
+    "url": "/polaris-ml/static/js/2.bd76062d.chunk.js"
+  },
+  {
+    "revision": "5aa85970dae6bc409bed18591ac972a6",
+    "url": "/polaris-ml/static/js/2.bd76062d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4824eb93bb95ce3bbccd",
+    "url": "/polaris-ml/static/js/main.65d96aab.chunk.js"
+  },
+  {
+    "revision": "7350c6078acb954d728e",
+    "url": "/polaris-ml/static/js/runtime-main.ec14345f.js"
+  }
+]);
